17b6c219e5a046865083773938d9acd6125773bf
Abdul-Hakeem <2425404166@live.gctu.edu.gh>
2026-05-30 20:12:07 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+#File: initials.py
+#Description: Print out my initials
+#Assignment Number: 2
+#
+#Name: Abdul-Hakeem Issah
+#STUDENT ID: 2425404166
+#Email: 2425404166@live.gctu.edu.gh
+#Grader: Augustus Buckman
+#
+#On my honor, Abdul-Hakeem Issah, this programming assignment is my own work
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+#printing my initials A,H,I
+    print()
+    print("...AHI")
+    print()
 
-
-
+    print(".......AAAA............HHHH....HHHH........IIIIIIIIIIII...**")
+    print("......AAAAAA...........HHHH....HHHH........IIIIIIIIIIII...**")
+    print(".....AAA..AAA..........HHHH....HHHH............IIII.......**")
+    print("....AAA....AAA.........HHHH....HHHH............IIII.......**")
+    print("...AAAAAAAAAAAA........HHHHHHHHHHHH............IIII.......**")
+    print("...AAAAAAAAAAAA........HHHHHHHHHHHH............IIII.......**")
+    print("...AAAA....AAAA........HHHH....HHHH............IIII.......**")
+    print("...AAAA....AAAA........HHHH....HHHH............IIII.......**")
+    print("...AAAA....AAAA...**...HHHH....HHHH...**...IIIIIIIIIIII...**")
+    print("...AAAA....AAAA...**...HHHH....HHHH...**...IIIIIIIIIIII...**")
+    print()
 main()
